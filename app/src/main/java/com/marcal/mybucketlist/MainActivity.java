99dfd78d1fd9ce044e74c71eec0f9c8fbee384b3
Marc <kemar79@qq.com>
2025-10-03 10:33:19 +0100

    Add listeners to navigate to the new activities #5

--- app/src/main/java/com/marcal/mybucketlist/MainActivity.java
+++ app/src/main/java/com/marcal/mybucketlist/MainActivity.java
@@ -1,9 +1,12 @@
 package com.marcal.mybucketlist;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -14,6 +17,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupClickListeners();
+    }
+
+    private void setupClickListeners() {
+        CardView thingsToDoCard = findViewById(R.id.card_view_things_to_do);
+        CardView placesToGoCard = findViewById(R.id.card_view_places_to_go);
+
+        thingsToDoCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ThingsToDoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        placesToGoCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PlacesToGoActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
